0bd96ef8491b02ad0f5d03c85ef2fd02be1dd115
Abraham Melquisedeque Pereira Licá <103894033+AbrahamLica@users.noreply.github.com>
2024-09-10 15:48:25 -0300

    more
    more

--- src/app/util/top-bar/top-bar.component.ts
+++ src/app/util/top-bar/top-bar.component.ts
@@ -1,14 +1,30 @@
 import { Component } from '@angular/core';
 import { ToolbarModule } from 'primeng/toolbar';
 import { AvatarModule } from 'primeng/avatar';
+import { ImageModule } from 'primeng/image';
+import { Router, RouterModule } from '@angular/router';
+import { SidebarModule } from 'primeng/sidebar';
 
 @Component({
   selector: 'app-top-bar',
   standalone: true,
-  imports: [ToolbarModule, AvatarModule],
+  imports: [ToolbarModule, AvatarModule, ImageModule, RouterModule, SidebarModule],
   templateUrl: './top-bar.component.html',
   styleUrl: './top-bar.component.scss'
 })
 export class TopBarComponent {
+
+  visibleSidebar: boolean = false;
+
+  constructor(private router: Router,) {}
+
+  goHome() {
+    this.router.navigateByUrl('/home');
+  }
+
+  irParaPainelJornalista() {
+    console.log("teste");
+    
+  }
 
 }
