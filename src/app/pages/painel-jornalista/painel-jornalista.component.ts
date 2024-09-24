5f63d990296cf1ea600c9d79be574601af361ef5
Abraham Melquisedeque Pereira Licá <103894033+AbrahamLica@users.noreply.github.com>
2024-09-24 11:20:33 -0300

    moer
    more

--- src/app/pages/painel-jornalista/painel-jornalista.component.ts
+++ src/app/pages/painel-jornalista/painel-jornalista.component.ts
@@ -14,12 +14,12 @@ import _ from 'lodash';
 import { AlertService } from 'src/app/services/alert.service';
 import { ConfirmModalComponent } from 'src/app/util/confirm-modal/confirm-modal.component';
 import { MessageService } from 'primeng/api';
-import { MessagesModule } from 'primeng/messages';
+import { ToastModule } from 'primeng/toast';
 
 @Component({
   selector: 'app-painel-jornalista',
   standalone: true,
-  imports: [DataViewModule, DataViewComponent, ImageModule, CommonModule, ToolbarModule, ButtonModule, MessagesModule],
+  imports: [DataViewModule, DataViewComponent, ImageModule, CommonModule, ToolbarModule, ButtonModule, ToastModule],
   providers: [DialogService, DynamicDialogRef, AlertService, DynamicDialogConfig, MessageService],
   templateUrl: './painel-jornalista.component.html',
   styleUrl: './painel-jornalista.component.scss',
@@ -48,6 +48,7 @@ export class PainelJornalistaComponent {
       if (resposta) {
         await this.apiService.makeDeleteRequest(`noticias/${item.id}`);
         this.noticias = this.noticias.filter((value) => value.id != item.id);
+        this.messageService.add({ severity: 'success', summary: 'Notícia excluída com sucesso!', detail: 'InfoPulse', icon: 'pi-check', key: 'tc', life: 3000 });
       }
     });
   }
@@ -65,7 +66,6 @@ export class PainelJornalistaComponent {
       if (noticia) {
         // @ts-ignore
         let noticiaSaved: any = noticia.id ? await this.apiService.makePutRequest('noticias/' + noticia.id, noticia) : await this.apiService.makePostRequest('noticias', noticia);
-        this.messageService.add({ severity: 'success', summary: 'Service Message', detail: 'Via MessageService' });
 
         if (!noticiaSaved) {
           this.criarNoticia(noticia);
@@ -74,11 +74,53 @@ export class PainelJornalistaComponent {
             this.noticias = this.noticias.map((value: Noticia) => {
               return value.id == noticia.id ? noticia : value;
             });
+            this.messageService.add({ severity: 'success', summary: 'Notícia editada com sucesso!', detail: 'InfoPulse', icon: 'pi-check', key: 'tl', life: 3000 });
           } else {
             this.noticias = [...this.noticias, noticiaSaved];
+            this.messageService.add({ severity: 'success', summary: 'Notícia criada com sucesso!', detail: 'InfoPulse', icon: 'pi-check', key: 'tl', life: 3000 });
           }
         }
       }
     });
+  }
+
+  toLocalDate(data: any) {
+    if (data instanceof Date) {
+      data.setUTCHours(3);
+      return data.toLocaleDateString('pt-BR', { timeZone: 'America/Belem' });
+    } else {
+      console.error('Data não é uma instância de Date', data);
+      return null;
+    }
+  }
+
+  extractDateOnly(dateTime: any): string {
+    if (!dateTime) {
+      return '';
+    }
+
+    // Se for um objeto Date, converte para string ISO
+    if (dateTime instanceof Date) {
+      dateTime = dateTime.toISOString().split('T')[0];
+    }
+
+    // Se ainda não for string, converte para string
+    if (typeof dateTime !== 'string') {
+      dateTime = String(dateTime);
+    }
+
+    // Verifica se é uma string no formato ISO e extrai a data
+    let datePart = '';
+    if (dateTime.includes('T')) {
+      datePart = dateTime.split('T')[0];
+    } else if (dateTime.includes(' ')) {
+      datePart = dateTime.split(' ')[0];
+    } else {
+      datePart = dateTime;
+    }
+
+    // Converte de yyyy-mm-dd para dd/mm/yyyy
+    const [year, month, day] = datePart.split('-');
+    return `${day}/${month}/${year}`;
   }
 }
