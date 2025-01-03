ccf4b32e14c7c99f62cf387e51858b279f265e13
Abraham Melquisedeque Pereira Licá <103894033+AbrahamLica@users.noreply.github.com>
2025-01-03 12:57:55 -0300

    more
    more

--- src/app/util/top-bar/top-bar.component.ts
+++ src/app/util/top-bar/top-bar.component.ts
@@ -21,7 +21,20 @@ import { ApiService } from 'src/app/services/api.service';
 @Component({
   selector: 'app-top-bar',
   standalone: true,
-  imports: [ToolbarModule, MenuModule, AvatarModule, ImageModule, RouterModule, SidebarModule, ButtonModule, RippleModule, StyleClassModule, InputSwitchModule, FormsModule, CommonModule],
+  imports: [
+    ToolbarModule,
+    MenuModule,
+    AvatarModule,
+    ImageModule,
+    RouterModule,
+    SidebarModule,
+    ButtonModule,
+    RippleModule,
+    StyleClassModule,
+    InputSwitchModule,
+    FormsModule,
+    CommonModule,
+  ],
   templateUrl: './top-bar.component.html',
   styleUrl: './top-bar.component.scss',
 })
@@ -38,52 +51,57 @@ export class TopBarComponent {
   sidebarVisible: boolean = false;
   isDarkMode = false;
 
-  constructor(private dialogService: DialogService, private router: Router, private usuarioService: UsuarioService, private apiService: ApiService) {
-
+  constructor(
+    private dialogService: DialogService,
+    private router: Router,
+    private usuarioService: UsuarioService,
+    private apiService: ApiService
+  ) {
     if (this.linkElement.href.includes('light')) {
-      this.isDarkMode = false
+      this.isDarkMode = false;
     } else {
-      this.isDarkMode = true
+      this.isDarkMode = true;
     }
-    
 
-    let usuario: any = ""
+    let usuario: any = '';
 
     this.apiService
-    .makeGetRequest(
-      `usuarios?size=99999&userId.equals=${this.usuarioService.getDadosUsuario()?.user?.id}`
-    )
-    .subscribe({
-      next: async (response: any) => {
-        usuario = response[0];
-        
-        this.items = [
-          {
-            label: usuario?.nome,
-            items: [
-              {
-                label: 'Painel do usuário',
-                icon: 'pi pi-user',
-                command: () => {
-                  this.irParaPainelUsuario();
-                },
-              },
-              {
-                label: 'Deslogar',
-                icon: 'pi pi-sign-out',
-                command: () => {
-                  this.usuarioService.deslogar();
-                },
-              },
-            ],
-          },
-        ];
-      }
-    })
+      .makeGetRequest(
+        `usuarios?size=99999&userId.equals=${
+          this.usuarioService.getDadosUsuario()?.user?.id
+        }`
+      )
+      .subscribe({
+        next: async (response: any) => {
+          usuario = response[0];
 
-    
-
-   
+          this.items = [
+            {
+              label: usuario?.nome,
+              items: [
+                {
+                  label: 'Painel do usuário',
+                  icon: 'pi pi-user',
+                  command: () => {
+                    this.irParaPainelUsuario();
+                  },
+                },
+                {
+                  label: 'Deslogar',
+                  icon: 'pi pi-sign-out',
+                  command: () => {
+                    this.usuarioService.deslogar();
+                  },
+                },
+              ],
+            },
+          ];
+        },
+        error: (e) => {
+          console.log(e);
+          
+        }
+      });
   }
 
   irParaPainelUsuario() {
@@ -101,11 +119,10 @@ export class TopBarComponent {
   }
 
   toggleLightDark() {
-    
     if (this.linkElement.href.includes('light')) {
       this.linkElement.href = 'theme-dark.css';
       this.isDarkMode = true;
-    } else if (this.linkElement.href.includes('dark')){
+    } else if (this.linkElement.href.includes('dark')) {
       this.linkElement.href = 'theme-light.css';
       this.isDarkMode = false;
     }
